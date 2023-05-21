33716ac41d1c71197c1a6e796c5a481cdd8c4a38
orangesummertime <ckf_xn@163.com>
2023-05-21 23:27:36 +0800

    Site updated: 2023-05-21 23:27:36

---
+++ HTML/KamiDrop/init.js
@@ -0,0 +1,6 @@
+const { Query, User } = AV;
+AV.init({
+    appId: "9QhTxYOocreAF0NQXn0QNHYN-gzGzoHsz",
+    appKey: "XzMZM7N4ZQiEf761FrxeKv1m",
+    serverURL: "https://9qhtxyoo.lc-cn-n1-shared.com"
+});
